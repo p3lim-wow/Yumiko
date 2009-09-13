a7687702d33e686e1873efece74559cee4cf5c2f
Adrian L Lange <adrianlund@gmail.com>
2009-09-13 23:07:43 +0200

    Add more checks, for safety

--- Yumiko.lua
+++ Yumiko.lua
@@ -2,9 +2,11 @@ local addon = CreateFrame('Frame')
 addon:RegisterEvent('PLAYER_TARGET_CHANGED')
 addon:SetScript('OnEvent', function()
 	for index = 1, GetNumTrackingTypes() do
-		local name, texture = GetTrackingInfo(index)
-		if(UnitExists('target') and string.find(name, UnitCreatureType('target'))) then
-			if(GetTrackingTexture(index) ~= texture) then
+		if(UnitExists('target') and UnitIsEnemy('player', 'target')) then
+			local name, texture = GetTrackingInfo(index)
+			local ctype = UnitCreatureType('target')
+
+			if(ctype and string.find(name, ctype) and GetTrackingTexture(index) ~= texture) then
 				SetTracking(index)
 			end
 		end
